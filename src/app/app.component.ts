b987353ccf656a4f0143c891bb2fc29a6304e0c5
Psaruklily <psarukli96@gmail.com>
2021-06-03 12:52:44 +0300

    Created buttons and added functionality to the button Start/Stop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stopwatch';
+
+  isToggled: boolean = false;
+  time = new Date(null);
+  interval;
+
+  startTimer():void {
+    this.interval = setInterval(() => {
+      this.time.setSeconds(this.time.getSeconds() + 1);
+    }, 1000)
+  }
+
+  stopTimer():void {
+    clearInterval(this.interval);
+  }
+
+  toggleStartStop():void {
+    this.isToggled = !this.isToggled;
+  
+    if (this.isToggled) {
+      this.startTimer();
+    } else {
+      this.stopTimer();
+    }
+  }
+
 }
