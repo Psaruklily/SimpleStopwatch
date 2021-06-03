c6ff96ef275de2eed570b367074e65ee8f640cd5
Psaruklily <psarukli96@gmail.com>
2021-06-03 16:04:01 +0300

    Added functionality for the Wait button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, ComponentFactoryResolver } from '@angular/core';
+import { Observable, fromEvent } from 'rxjs';
+import { debounceTime, buffer, filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'stopwatch';
 
   isToggled: boolean = false;
   time = new Date(null);
   interval;
 
-  startTimer():void {
+  @ViewChild('btnWait', { static: true }) button: ElementRef;
+
+  ngOnInit() {
+    const clickStream = fromEvent(this.button.nativeElement, 'click');
+
+    const buff = clickStream.pipe(
+      debounceTime(250),
+    )
+    
+    const doubleClicks = clickStream.pipe(
+      buffer(buff),
+      map(list => {
+        return list.length;
+      }),
+      filter(x => x === 2),
+    )
+    
+    doubleClicks.subscribe(() => {
+      console.log('doubleclick');
+      this.stopTimer();
+      this.isToggled = false;
+    })
+  }
+  
+  startTimer(): void {
     this.interval = setInterval(() => {
       this.time.setSeconds(this.time.getSeconds() + 1);
     }, 1000)
   }
 
-  stopTimer():void {
+  stopTimer(): void {
     clearInterval(this.interval);
   }
 
-  toggleStartStop():void {
+  toggleStartStop(): void {
     this.isToggled = !this.isToggled;
-  
+
     if (this.isToggled) {
       this.startTimer();
     } else {
       this.stopTimer();
     }
   }
-
 }
